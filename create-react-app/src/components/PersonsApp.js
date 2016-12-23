3f0f553169087a8557c6cb97f16134a3f855cefe
alex.nerush <ale.nerush@gmail.com>
2016-12-24 00:23:55 +0200

    SearchBar

--- create-react-app/src/components/PersonsApp.js
+++ create-react-app/src/components/PersonsApp.js
@@ -1,11 +1,28 @@
 import React, {Component, PropTypes} from 'react';
 import PersonsList from './PersonsList';
+import SearchBar from './SearchBar';
 
 class PersonsApp extends Component {
+
+    constructor(props) {
+        super(props);
+        this.handleFilter = this.handleFilter.bind(this);
+    }
+
+    state = {
+        filterText: ''
+    }
+
+    handleFilter(filterText) {
+        this.setState({filterText});
+    }
+
     render(){
         return (
             <div className="persons-app">
                 <h1>Persons App</h1>
+                <SearchBar filterText={this.state.filterText} 
+                    onUserInput={this.handleFilter} />
                 <PersonsList persons={this.props.persons}/>
             </div>
         )
